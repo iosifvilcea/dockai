68b27ffffe3a88a68844341776818b4652228f21
iosifvilcea <iosifvilcea@protonmail.com>
2026-05-12 12:54:43 -0400

    Add docker socket access to dev container.

--- run.bash
+++ run.bash
@@ -7,4 +7,5 @@ docker run -it --rm \
   -v gradle_cache:/home/aiuser/.gradle \
   -v pip_cache:/home/aiuser/.cache/pip \
   -v "$(realpath "$TARGET_DIR")":/workspace \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   dockai
